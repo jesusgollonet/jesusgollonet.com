0eca3dad1fb9b39d75d2d403096cf68eae720ef4
Jesus Gollonet <jesusgollonet@gmail.com>
2013-04-29 00:35:18 +0200

    Added deploy settings.
    Avoiding uploading sensitive info by using a .gitignore'd local_env.file.
    Load it with a couple of helpers in config.rb and set ENV variables.

--- config.rb
+++ config.rb
@@ -18,7 +18,6 @@ data.projects.each do |p|
   end
 end
 
-
 # Build-specific configuration
 configure :build do  
   activate :minify_css
@@ -26,3 +25,33 @@ configure :build do
   activate :cache_buster
   activate :smusher # compressing images
 end
+
+helpers do
+  def get_local_env_path
+    env_file = File.join(File.dirname(__FILE__) , '', 'local_env.yml')
+    env_file
+  end
+
+  def set_env_vars file_path
+    YAML.load(File.open(file_path)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(file_path)
+  end
+end
+
+# sensible settings are stored in a .gitignored file called local_env.yml
+# and read as ENV variables below
+# http://railsapps.github.io/rails-environment-variables.html
+set_env_vars(get_local_env_path)
+
+# Activate deploy extension
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host =  ENV["HOST"]
+  deploy.user =  ENV["USER"]
+  deploy.path =  ENV["STAGING_PATH"]
+end
+
+
+
+
