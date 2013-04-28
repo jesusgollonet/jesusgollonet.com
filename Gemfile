e79ae5c385075aa0dccd30953d8f0aeaadaaaffa
Jesus Gollonet <jesusgollonet@gmail.com>
2013-04-28 21:14:35 +0200

    Added Gemfile, require gems, clean config.rb in order to be able to build

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'http://rubygems.org'
 
 gem "middleman", "~>3.0.13"
+gem "middleman-deploy", "~> 0.0.11"
+gem "middleman-smusher", "~> 3.0.0"
+gem "slim", "~> 1.3.5"
+gem "redcarpet", "~> 2.2.2"
+gem "pry"
--- config.rb
+++ config.rb
@@ -1,113 +1,28 @@
-### 
-# Compass
-###
 
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
+require 'slim'
+require 'redcarpet'
+require "middleman-smusher"
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Haml
-###
-set :slim, :pretty => true
-
+# set :slim, :pretty => true
 set :markdown, :layout_engine => :slim
 set :markdown_engine, :redcarpet
 
-activate :directory_indexes
 
+activate :directory_indexes 
 
-ignore "/code.html"
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Page command
-###
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
 
 # generate dynamic pages for each project. the variable @project will carry the yaml data structure
 data.projects.each do |p|
-  page "/projects/#{p.url}.html", :proxy => "/projects/project.html" do
-    # maybe I can construct links and imgs here? (for the long texts and )
-    
-    # should the view now the elements of project or should i pass explicitly?
+  proxy "/projects/#{p.url}.html", "project.html", :ignore => true do
     @project = p
-    
-    # can I interpret markdown here?
-    # @rendered = Markdown.render "This is *bongos*, indeed."
   end
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
 
 # Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-  
-  # Minify Javascript on build
-  # activate :minify_javascript
-  
-  # Enable cache buster
-  # activate :cache_buster
-  
-  # Use relative URLs
-  # activate :relative_assets
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+configure :build do  
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  activate :smusher # compressing images
 end
