2f26417e5195a449519c2726ca2280300170755a
Jesus Gollonet <jesusgollonet@gmail.com>
2014-04-14 19:35:04 +0200

    Add a bunch of functions to preview + check if server is up

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 
 preview(){
-    (cd build
-    middleman && open http://localhost:4567)
+    if is_server_up
+    then
+        open http://localhost:4567
+    else
+        echo "no server is up. start preview"
+        (cd build
+        launch_server
+        check_localhost_server_is_up)
+    fi
+}
+
+launch_server(){
+    echo "launching middleman server in the background"
+    echo "run screen -r to see its output"
+    screen -d -m middleman 
 }
 
 build(){
     (cd build
     bundle install
     bundle exec middleman build)
+}
+
+check_localhost_server_is_up(){
+    while true 
+    do
+        echo "checking if server has booted"
+        if is_server_up
+        then
+            open http://localhost:4567
+            break
+        fi
+        sleep 1
+    done
+}
+
+is_server_up(){
+    if nmap -p 4567 localhost | grep open > /dev/null
+    then
+        echo "server is up!"
+        return 0
+    else
+        echo "server is down"
+        return 1
+    fi
 }
